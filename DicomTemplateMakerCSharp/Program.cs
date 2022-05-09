6ebf50909a7096e028edcaf31ef1cca451e1a354
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-05-09 16:50:20 -0700

    getting a feeling for DICOM RT in csharp

---
+++ DicomTemplateMakerCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FellowOakDicom;
+
+namespace DicomTemplateMakerCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string dicom_file = @"C:\Users\b5anderson\Modular_Projects\Dicom_RT_and_Images_to_Mask\src\DicomRTTool\template_RS.dcm";
+            var file = DicomFile.Open(dicom_file, FileReadOption.ReadAll);
+            string modality = file.Dataset.GetString(DicomTag.Modality);
+        }
+    }
+}
