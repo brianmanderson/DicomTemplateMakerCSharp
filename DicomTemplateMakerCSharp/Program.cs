3cbe1b6f56db9e741f1cd86b13d50d0ef9ecd956
brianmanderson <markba122@gmail.com>
2022-05-13 21:32:25 -0700

    going towards looking for template files and move onwards

--- DicomTemplateMakerCSharp/Program.cs
+++ DicomTemplateMakerCSharp/Program.cs
@@ -1,22 +1,44 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 using DicomTemplateMakerCSharp.Services;
 
 namespace DicomTemplateMakerCSharp
 {
     class Program
     {
+        string temp_folder;
         static void Main(string[] args)
         {
+            string roiname, color, interperter;
             Console.WriteLine("Hello World!");
-            string temp_folder = @"\\ucsdhc-varis2\radonc$\BMAnderson\BMA_Export";
-            temp_folder = @"C:\Users\markb\Modular_Projects\Example_Data\Data\Image_Data\T1\Post1\001";
+            string[] templates = Directory.GetFiles(@".", "*Template*.txt", SearchOption.AllDirectories);
             DicomSeriesReader reader = new DicomSeriesReader();
-            reader.parse_folder(temp_folder);
-            foreach (string uid in reader.dicomParser.dicom_series_instance_uids)
+            foreach (string template_file in templates)
             {
-                reader.load_DICOM(uid);
-                reader.update_template(true);
+                List<ROIClass> rois = new List<ROIClass>();
+                List<string> instructions = new List<string>();
+                string[] temp = File.ReadAllLines(template_file);
+                foreach (string t in temp)
+                {
+                    instructions.Add(t);
+                }
+                string temp_folder = instructions[0];
+                instructions.RemoveAt(0);
+                while (instructions.Count > 0)
+                {
+                    roiname = instructions[0];
+                    color = instructions[1];
+                    interperter = instructions[2];
+                }
+                reader.parse_folder(temp_folder);
+                foreach (string uid in reader.dicomParser.dicom_series_instance_uids)
+                {
+                    reader.load_DICOM(uid);
+                    reader.update_template(true);
+                }
             }
+
             
         }
     }
--- DicomTemplateMakerCSharp/Services/ROIClass.cs
+++ DicomTemplateMakerCSharp/Services/ROIClass.cs
@@ -14,9 +14,11 @@ namespace DicomTemplateMakerCSharp.Services
         string roi_interpreted_type;
         // reference identifies the structure set ROI sequence
         // observation_number unique within observation sequence
-        public ROIClass(string color, string name)
+        public ROIClass(string color, string name, string roi_interpreted_type)
         {
             this.name = name;
+            this.color = color;
+            this.roi_interpreted_type = roi_interpreted_type;
         }
     }
 }
