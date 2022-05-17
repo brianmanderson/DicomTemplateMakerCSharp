5aca93dabe94b06a5917ca63c18a3bf590e4fd5c
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-05-17 13:09:32 -0700

    making the GUI now

---
+++ DicomTemplateMakerGUI/Windows/MakeTemplateWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.IO;
+using System.Windows.Data;
+using System.Threading;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using System.Threading.Tasks;
+namespace DicomTemplateMakerGUI.Windows
+{
+    /// <summary>
+    /// Interaction logic for MakeTemplateWindow.xaml
+    /// </summary>
+    public partial class MakeTemplateWindow : Window
+    {
+        string dicom_file;
+        public MakeTemplateWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Select_File_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = ".";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                dicom_file = dialog.FileName;
+                FileLocationLabel.Content = dicom_file;
+                BuildButton.IsEnabled = true;
+            }
+        }
+
+        private void Build_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
