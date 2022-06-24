d4a16d10d4ca847af32dbf0743c92cffa3f345d6
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-06-24 12:11:24 -0700

    turn adding keys into a singel function

--- ReadAirTable.py
+++ ReadAirTable.py
@@ -1,6 +1,22 @@
 import os
-import pydicom
 from matplotlib.colors import to_rgb
+import pydicom
+
+
+def add_to_base_dictionary(out_dictionary, fields):
+    color_name = fields['Jeff Colors'][0].replace(' ', '')
+    RGB = [int(i*255) for i in to_rgb(color_name)]
+    color = f"{RGB[0]}\{RGB[1]}\{RGB[2]}"
+    structure_fields = {'Name': fields['Structure'], 'Color': color,
+                        'Type': fields['Type'], 'FMAID': fields['FMAID']}
+    for group in fields.keys():
+        if fields[group] in ['Consider', 'Recommend']:
+            if group not in out_dictionary:
+                out_dictionary[group] = []
+            out_dictionary[group].append(structure_fields)
+    return None
+
+
 fid = open(os.path.join('.', 'API_Key.txt'))
 API_KEY = fid.readline()
 fid.close()
@@ -13,18 +29,12 @@ from pyairtable import Api, Base, Table
 base = Base(API_KEY, BASE_KEY)
 table = base.get_table(f"{TABLE_KEY}")
 out_dictionary = {}
+problem_fields = []
 all_records = table.all()
 for record in all_records:
     fields = record['fields']
-    fmaid = fields['FMAID']
-    color_name = fields['Jeff Colors'][0].replace(' ', '')
-    RGB = [int(i*255) for i in to_rgb(color_name)]
-    color = f"{RGB[0]}\{RGB[1]}\{RGB[2]}"
-    structure_fields = {'Name': fields['Structure'], 'Color': color,
-                        'Type': fields['Type'], 'FMAID': fields['FMAID']}
-    for group in fields.keys():
-        if fields[group] in ['Consider', 'Recommend']:
-            if group not in out_dictionary:
-                out_dictionary[group] = []
-            out_dictionary[group].append(structure_fields)
+    try:
+        add_to_base_dictionary(out_dictionary, fields)
+    except:
+        problem_fields.append(fields)
 xxx = 1
