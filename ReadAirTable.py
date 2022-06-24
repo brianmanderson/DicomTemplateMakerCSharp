3ae6084b9998255e5db447cf06df0765738eb1de
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-06-24 09:21:41 -0700

    can now read the tables, trying to figure out the sheet names next

--- ReadAirTable.py
+++ ReadAirTable.py
@@ -1,13 +1,20 @@
 import os
 import pydicom
 fid = open(os.path.join('.', 'API_Key.txt'))
-api_key = fid.readline()
+API_KEY = fid.readline()
 fid.close()
+TABLE_KEY = 'tblex7IPsmm8hvVEc'
+BASE_KEY = 'appTUL6ZaSepTawFw'
 fid = open(os.path.join('.', 'Base_key.txt'))
 base_id = fid.readline()
 fid.close()
 from pyairtable import Api, Base, Table
-base = Base(api_key, base_id)
-table = base.get_table('General')
-table.all()
+base = Base(API_KEY, BASE_KEY)
+table = base.get_table(f"{TABLE_KEY}")
+all_records = table.all()
+for record in all_records:
+    fields = record['fields']
+    name = fields['Structure']
+    fmaid = fields['FMAID']
+    
 xxx = 1
