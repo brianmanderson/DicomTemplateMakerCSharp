d1680134bcbc8fa6c51735de09072381e5087aee
brianmanderson <markba122@gmail.com>
2022-06-26 18:00:32 -0700

    making it so we can add and edit ontologies as needed

---
+++ DicomTemplateMakerGUI/StackPanelClasses/AddOntologyRow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DicomTemplateMakerGUI.Services;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Collections.Generic;
+
+namespace DicomTemplateMakerGUI.StackPanelClasses
+{
+    public class OntologyClass
+    {
+        public string Name;
+    }
+    class AddOntologyRow : StackPanel
+    {
+        private OntologyClass ontology;
+        private TextBox ontology_name_textbox;
+        private CheckBox DeleteCheckBox;
+        private Button DeleteButton;
+        private OntologyClass onto;
+        public AddOntologyRow(OntologyClass ontology)
+        {
+            this.onto = ontology;
+            ontology_name_textbox = new TextBox();
+            ontology_name_textbox.Text = onto.Name;
+            ontology_name_textbox.TextChanged += OntologyNameChanged;
+        }
+        private void OntologyNameChanged(object sender, TextChangedEventArgs e)
+        {
+            onto.Name = ontology_name_textbox.Text;
+        }
+    }
+}
