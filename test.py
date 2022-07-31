db94c0d6893868cbab9511a461da2449310a9951
brianmanderson <markba122@gmail.com>
2022-07-31 09:16:40 -0700

    updates

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pydicom
 # from DicomRTTool.ReaderWriter import DicomReaderWriter
 # from PlotScrollNumpyArrays.Plot_Scroll_Images import plot_scroll_Image
 
-ds = pydicom.read_file(r'C:\Users\markb\Modular_Projects\DicomTemplateMakerCSharp\DicomTemplateMakerGUI\TG263_Abdomen.dcm')
+ds = pydicom.read_file(r'C:\Users\markb\Modular_Projects\DicomTemplateMakerCSharp\DicomTemplateMakerGUI\TG263_Breast.dcm')
 reader = DicomReaderWriter(Contour_Names=['Eye_R'])
 reader.walk_through_folders(r'\\ucsdhc-varis2\Radonc$\DICOM\BMA_Export')
 reader.get_mask()
