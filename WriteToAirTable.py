b794918501dae9c5938d1edc7f1b36695389c8d0
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-08-05 14:23:43 -0700

    making a UCSD style airtable for each site

--- WriteToAirTable.py
+++ WriteToAirTable.py
@@ -24,21 +24,66 @@ def add_to_base_dictionary(out_dictionary, fields):
 fid = open(os.path.join('.', 'API_Key.txt'))
 API_KEY = fid.readline()
 fid.close()
-fid = open(os.path.join('.', 'Base_key.txt'))
+fid = open(os.path.join('.', 'Test_Base_key.txt'))
 BASE_KEY = fid.readline()
 fid.close()
 TABLE_KEY = 'tblex7IPsmm8hvVEc'
-#fid = open(os.path.join('.', 'Test_Table_key.txt'))
-#TABLE_KEY = fid.readline()
-#fid.close()
+fid = open(os.path.join('.', 'Test_Table_key.txt'))
+TABLE_KEY = fid.readline()
+fid.close()
 from pyairtable import Api, Base, Table
 base = Base(API_KEY, BASE_KEY)
 table = base.get_table(f"{TABLE_KEY}")
-out_dictionary = {}
-problem_fields = []
 all_records = table.all()
-ptv_ctv_gtv_records = []
+all_records_dict = {}
+current_dict = {}
 for record in all_records:
-    if record['fields']['Type'] in ['CTV', 'GTV', 'PTV']:
-        ptv_ctv_gtv_records.append(record)
-table.update(ptv_ctv_gtv_records[1]['id'], {'FMAID': 88}) # Can update a single value, etc.
+    current_dict[record['fields']['FMAID']] = {'Sites': record['fields']['Sites'], 'id': record['id']}
+    all_records_dict[record['id']] = record['fields']
+
+path = r'O:\DICOM\BMA_Export'
+ds = pydicom.read_file(os.path.join(path, 'Prostate.dcm'))
+site = 'Prostate'
+temp_out_dict = {}
+for roi_observation in ds.RTROIObservationsSequence:
+    test_dict = {'CommonName': None, 'FMAID': None, 'Type': None, 'RGB': None, 'Scheme': None,
+                 'ContextGroupVersion': None, 'MappingResource': None,
+                 'ContextIdentifier': None, 'MappingResourceName': None, 'MappingResourceUID': None,
+                 'ContextUID': None, 'Sites': None}
+    test_dict['Type'] = roi_observation.RTROIInterpretedType
+    if test_dict['Type'] == "":
+        test_dict['Type'] = "AVOIDANCE"
+    rt_code = roi_observation.RTROIIdentificationCodeSequence[0]
+    test_dict['FMAID'] = rt_code.CodeValue
+    test_dict['Sites'] = [site]
+    if rt_code.CodeValue in current_dict:
+        if site not in current_dict[rt_code.CodeValue]['Sites']:
+            test_dict['Sites'] = current_dict[rt_code.CodeValue]['Sites'] + [site]
+    test_dict['CommonName'] = rt_code.CodeMeaning
+    test_dict['Scheme'] = rt_code.CodingSchemeDesignator
+    test_dict['ContextIdentifier'] = rt_code.ContextIdentifier
+    test_dict['ContextGroupVersion'] = rt_code.ContextGroupVersion
+    test_dict['ContextUID'] = rt_code.ContextUID
+    test_dict['MappingResource'] = rt_code.MappingResource
+    test_dict['MappingResourceName'] = rt_code.MappingResourceName
+    test_dict['MappingResourceUID'] = rt_code.MappingResourceUID
+    temp_out_dict[roi_observation.ReferencedROINumber] = test_dict
+for roi in ds.ROIContourSequence:
+    if roi.ReferencedROINumber in temp_out_dict:
+        color = roi.ROIDisplayColor
+        temp_out_dict[roi.ReferencedROINumber]['RGB'] = f"{color[0]},{color[1]},{color[2]}"
+out_list = []
+fmaids = []
+for dictionary in temp_out_dict.values():
+    if dictionary['FMAID'] not in fmaids:
+        fmaids.append(dictionary['FMAID'])
+        out_list.append(dictionary)
+to_write = []
+for dictionary in out_list:
+    if dictionary['FMAID'] in current_dict:
+        if all_records_dict[current_dict[dictionary['FMAID']]['id']] != dictionary:
+            table.update(current_dict[dictionary['FMAID']]['id'], dictionary, typecast=True)
+    else:
+        to_write.append(dictionary)
+if to_write:
+    table.batch_create(to_write, typecast=True)
