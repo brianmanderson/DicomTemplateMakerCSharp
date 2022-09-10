9ab2c2af12b52456e9ab90be873d6ee7c4f358b8
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-09-09 17:01:40 -0700

    'evening_update'

--- DicomTemplateMakerGUI/MainWindow.xaml.cs
+++ DicomTemplateMakerGUI/MainWindow.xaml.cs
@@ -58,6 +58,7 @@ namespace DicomTemplateMakerGUI
         string folder_location, onto_path;
         Brush lightgreen = new SolidColorBrush(Color.FromRgb(144, 238, 144));
         Brush lightgray = new SolidColorBrush(Color.FromRgb(221, 221, 221));
+        List<ReadAirTable> airtables = new List<ReadAirTable>();
         ReadAirTable airtable;
         bool running;
         DicomRunner runner;
