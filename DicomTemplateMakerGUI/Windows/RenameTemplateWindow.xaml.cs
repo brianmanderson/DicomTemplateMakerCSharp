36fbfe65cdbbd08ef2172fa7ae77329a698469dd
brianmanderson <markba122@gmail.com>
2022-11-20 17:24:12 -0800

    added textboxes and buttons for renaming

---
+++ DicomTemplateMakerGUI/Windows/RenameTemplateWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DicomTemplateMakerGUI.Windows
+{
+    /// <summary>
+    /// Interaction logic for RenameTemplateWindow.xaml
+    /// </summary>
+    public partial class RenameTemplateWindow : Window
+    {
+        public RenameTemplateWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rename_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
