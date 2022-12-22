89c22886029bece93fa10a11629cec760a0ae0bb
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-12-22 15:42:17 -0800

    adding a xamlmaker csharp class

---
+++ Xaml_Maker/XamlMakerCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XamlMakerCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
