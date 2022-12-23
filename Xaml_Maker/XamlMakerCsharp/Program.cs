01ae32ec4d66766cd44f01daf781e419b9910569
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-12-22 16:10:39 -0800

    can now write in xaml!

--- Xaml_Maker/XamlMakerCsharp/Program.cs
+++ Xaml_Maker/XamlMakerCsharp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+using System.IO;
 
 namespace XamlMakerCsharp
 {
@@ -6,7 +10,17 @@ namespace XamlMakerCsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            XDocument doc = XDocument.Load(Path.Combine(@".", "Structure Template.xml"));
+            XElement root = doc.Root;
+            XElement base_struct = root.Element("Structures");
+            XElement Structure = new XElement(base_struct.Element("Structure"));
+            Structure.SetAttributeValue("ID", "Test");
+            Structure.SetAttributeValue("Name", "Test");
+
+            XElement Identification = Structure.Element("Identification");
+            Identification.SetAttributeValue("VolumeType", "Test");
+            base_struct.Add(Structure);
+            int x = 1;
         }
     }
 }
