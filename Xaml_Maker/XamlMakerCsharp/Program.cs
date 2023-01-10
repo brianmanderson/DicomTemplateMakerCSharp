a8d5619c27827e60b3a072c7246e2aeb56acd8db
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2023-01-10 14:20:08 -0800

    created a class for the Xaml writer

--- Xaml_Maker/XamlMakerCsharp/Program.cs
+++ Xaml_Maker/XamlMakerCsharp/Program.cs
@@ -4,6 +4,7 @@ using System.Xml.Linq;
 using System.Linq;
 using System.IO;
 using System.Runtime;
+using XamlMakerCsharp;
 
 namespace XamlMakerCsharp
 {
@@ -11,87 +12,15 @@ namespace XamlMakerCsharp
     {
         static void Main(string[] args)
         {
-            XNamespace ab = "http://www.w3.org/2001/XMLSchema-instance";
+            string rois_path = @"C:\Users\b5anderson\Modular_Projects\DicomTemplateMakerCSharp\DicomTemplateMakerGUI\bin\x64\Debug";
+            foreach (string directory in Directory.GetDirectories(rois_path))
+            {
+                VarianXmlWriter xmlwriter = new VarianXmlWriter();
 
-            XDocument doc = XDocument.Load(Path.Combine(@".", "Structure Template.xml"));
-            XElement root = doc.Root;
-            XElement preview = root.Element("Preview");
-            preview.SetAttributeValue("AssignedUsers", "b5anderson");
-            preview.SetAttributeValue("ID", "Test TemplateBMA");
-            DateTime now = DateTime.Now;
-            preview.SetAttributeValue("ApprovalHistory", $"b5anderson Created [ {now.Month} {now.Day} {now.Year} {now.Hour}:{now.Minute}:{now.Second}]");
-            preview.SetAttributeValue("Description", "Auto-generated xml file");
-            preview.SetAttributeValue("LastModified", $"[ {now.Month} {now.Day} {now.Year} {now.Hour}:{now.Minute}:{now.Second}]");
+                xmlwriter.LoadROIsFromPath(directory);
+                xmlwriter.SaveFile($@"K:\{Path.GetFileName(directory)}.xml");
+            }
 
-            XElement base_struct = root.Element("Structures");
-            base_struct.RemoveAll();
-            XElement new_structure = new XElement("Structure");
-            new_structure.SetAttributeValue("ID", "Test");
-            new_structure.SetAttributeValue("Name", "Test");
-
-            XElement Identification = new XElement("Identification");
-            Identification.Add(new XElement("VolumeID"));
-            Identification.Add(new XElement("VolumeCode"));
-            XElement VolumeType = new XElement("VolumeType");
-            VolumeType.Value = "PTV";
-            Identification.Add(VolumeType);
-            Identification.Add(new XElement("VolumeCodeTable"));
-
-            XElement StructureCode = new XElement("StructureCode");
-            StructureCode.SetAttributeValue("Code", "123");
-            StructureCode.SetAttributeValue("CodeScheme", "FMA");
-            StructureCode.SetAttributeValue("CodeSchemeVersion", "3.2");
-            Identification.Add(StructureCode);
-            new_structure.Add(Identification);
-
-            XElement TypeIndex = new XElement("TypeIndex");
-            TypeIndex.Value = "3";
-            new_structure.Add(TypeIndex);
-
-            XElement ColorAndStyle = new XElement("ColorAndStyle");
-            ColorAndStyle.Value = "Blue";
-            new_structure.Add(ColorAndStyle);
-
-            XElement SearchCTLow = new XElement("SearchCTLow");
-            SearchCTLow.SetAttributeValue(ab + "nil", "true");
-            new_structure.Add(SearchCTLow);
-
-            XElement SearchCTHigh = new XElement("SearchCTHigh");
-            SearchCTHigh.SetAttributeValue(ab + "nil", "true");
-            new_structure.Add(SearchCTHigh);
-
-            XElement DVHLineStyle = new XElement("DVHLineStyle");
-            DVHLineStyle.Value = "0";
-            new_structure.Add(DVHLineStyle);
-
-            XElement DVHLineColor = new XElement("DVHLineColor");
-            DVHLineColor.Value = "-16777216";
-            new_structure.Add(DVHLineColor);
-
-            XElement DVHLineWidth = new XElement("DVHLineWidth");
-            DVHLineWidth.Value = "1";
-            new_structure.Add(DVHLineWidth);
-
-            XElement EUDAlpha = new XElement("EUDAlpha");
-            EUDAlpha.SetAttributeValue(ab + "nil", "true");
-            new_structure.Add(EUDAlpha);
-
-            XElement TCPAlpha = new XElement("TCPAlpha");
-            TCPAlpha.SetAttributeValue(ab + "nil", "true");
-            new_structure.Add(TCPAlpha);
-
-            XElement TCPBeta = new XElement("TCPBeta");
-            TCPBeta.SetAttributeValue(ab + "nil", "true");
-            new_structure.Add(TCPBeta);
-
-            XElement TCPGamma = new XElement("TCPGamma");
-            TCPGamma.SetAttributeValue(ab + "nil", "true");
-            new_structure.Add(TCPGamma);
-
-            base_struct.Add(new_structure);
-            XmlWriter writer = XmlWriter.Create(@"K:\test.xml");
-            doc.WriteTo(writer);
-            writer.Close();
             int x = 1;
         }
     }
