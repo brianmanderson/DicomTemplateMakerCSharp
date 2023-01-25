529157c7ae051f31cfa55ca3f6013e9af570f2a3
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2023-01-24 16:02:20 -0800

    making a reader now, go to the opposite direction

--- Xaml_Maker/XamlMakerCsharp/Program.cs
+++ Xaml_Maker/XamlMakerCsharp/Program.cs
@@ -12,14 +12,29 @@ namespace XamlMakerCsharp
     {
         static void Main(string[] args)
         {
-            string rois_path = @"C:\Users\b5anderson\Modular_Projects\DicomTemplateMakerCSharp\DicomTemplateMakerGUI\bin\x64\Debug";
-            foreach (string directory in Directory.GetDirectories(rois_path))
+            if (true)
             {
-                VarianXmlWriter xmlwriter = new VarianXmlWriter();
-
-                xmlwriter.LoadROIsFromPath(directory);
-                xmlwriter.SaveFile($@"K:\{Path.GetFileName(directory)}.xml");
+                string xml_path = @"\\ro-ariaimg-v\va_data$\ProgramData\Vision\Templates\structure";
+                string out_path = @"C:\Users\b5anderson\Modular_Projects\DicomTemplateMakerCSharp\DicomTemplateMakerGUI\bin\x64\Debug";
+                foreach (string file in Directory.GetFiles(xml_path, "*.xml"))
+                {
+                    string new_file = @"K:\Template_Output_VarianXml\AbdPelv_Anal.xml";
+                    VarianXmlReader reader = new VarianXmlReader(new_file);
+                    reader.XmlToROI(out_path);
+                }
             }
+            if (false)
+            {
+                string rois_path = @"C:\Users\b5anderson\Modular_Projects\DicomTemplateMakerCSharp\DicomTemplateMakerGUI\bin\x64\Debug";
+                foreach (string directory in Directory.GetDirectories(rois_path))
+                {
+                    VarianXmlWriter xmlwriter = new VarianXmlWriter();
+
+                    xmlwriter.LoadROIsFromPath(directory);
+                    xmlwriter.SaveFile($@"K:\{Path.GetFileName(directory)}.xml");
+                }
+            }
+
 
             int x = 1;
         }
---
+++ Xaml_Maker/XamlMakerCsharp/VarianXmlReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+using System.IO;
+using System.Runtime;
+
+namespace XamlMakerCsharp
+{
+    public class VarianXmlReader
+    {
+        private XNamespace ab = "http://www.w3.org/2001/XMLSchema-instance";
+        public XDocument doc;
+        public XElement root;
+        public XElement base_struct;
+        public XElement preview;
+        public VarianXmlReader(string xml_path)
+        {
+            doc = XDocument.Load(xml_path);
+            root = doc.Root;
+            preview = root.Element("Preview");
+        }
+        public void XmlToROI(string output_path)
+        {
+            //string description = preview.Elements("ID");
+            string template_name = preview.Attribute("ID").Value.Replace(' ', '_');
+            if (!Directory.Exists(Path.Combine(output_path, template_name)))
+            {
+                Directory.CreateDirectory(Path.Combine(output_path, template_name));
+            }
+            XElement structures = root.Element("Structures");
+            foreach (XElement structure in structures.Elements())
+            {
+                int x = 1;
+            }
+        }
+    }
+}
