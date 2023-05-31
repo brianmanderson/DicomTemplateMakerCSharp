c0184d181c22283ca89802b746855ce7dc18e1af
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2023-05-31 14:29:22 -0700

    updated things

--- CreateFMAToSNOMEDFile.py
+++ CreateFMAToSNOMEDFile.py
@@ -5,12 +5,12 @@ import os
 sheet_id = "1AC2wtYKqWXmQsb-_I1OJ6aW32--pQ4vPwFK-wX6L2II"
 sheet_name = "Sheet1"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-read_df = pd.read_csv(url)[["FMAID", "SNOMED-CT Code", "Checked?"]].dropna()
-values = read_df.to_numpy("int")
-out_path = r'FMAID To SNOMED_Updated.xlsx'
-out_dictionary = {"FMAID": [], "SNOMEDCT": []}
+read_df = pd.read_csv(url)[["FMAID", "SNOMED-CT Code", "Checked?", "Description"]].dropna()
+values = read_df.to_numpy()
+out_dictionary = {"FMAID": [], "SNOMEDCT": [], "Description": []}
 for index, single_pair in enumerate(values):
-    fmaid, snomed, checked = single_pair
+    fmaid, snomed, checked, description = single_pair
+    fmaid, snomed, checked = int(fmaid), int(snomed), int(checked)
     if checked != 1:
         continue
     if fmaid == 88 or type(fmaid) == str:
@@ -21,13 +21,14 @@ for index, single_pair in enumerate(values):
         if snomed not in out_dictionary["SNOMEDCT"]:
             out_dictionary["FMAID"].append(fmaid)
             out_dictionary["SNOMEDCT"].append(snomed)
-
+            out_dictionary["Description"].append(description)
 fid = open(os.path.join('.', 'DicomTemplateMakerGUI', "FMA_SNOMEDCT_Key.txt"), 'w+')
 fid.write('FMA, SNOMED-CT Code\n')
-for fmaid, snomed in zip(out_dictionary["FMAID"], out_dictionary["SNOMEDCT"]):
-    fid.write(f"{fmaid},{snomed}\n")
+for fmaid, snomed, desc in zip(out_dictionary["FMAID"], out_dictionary["SNOMEDCT"], out_dictionary["Description"]):
+    fid.write(f"{fmaid},{snomed},{desc}\n")
 fid.close()
 df = pd.DataFrame(out_dictionary)
+out_path = r'FMAID To SNOMED_Updated.xlsx'
 with pd.ExcelWriter(out_path) as writer:
     df.to_excel(writer, sheet_name="Updated", index=False)
 
--- test.py
+++ test.py
@@ -3,19 +3,8 @@ import os
 # from DicomRTTool.ReaderWriter import DicomReaderWriter
 # from PlotScrollNumpyArrays.Plot_Scroll_Images import plot_scroll_Image
 
-path = r'O:\DICOM\BMA_Export\Single_Image'
-dataset = pydicom.Dataset()
-dataset.PatientName = "Anderson^Brian"
+path = r'C:\Users\b5anderson\Downloads\SmallCTBreast'
 for file in os.listdir(path):
     ds = pydicom.read_file(os.path.join(path, file))
-    ds[0x0010, 0x0010] = pydicom.DataElement(0x00100010, "PN", "Brian Mark Anderson")
-    ds.PatientID = "0003141592654"
-    ds.SeriesDescription = "UCSD_Residency_Template_Patient"
+    ds.SeriesDescription = "Breast"
     ds.save_as(os.path.join(path, file))
-    xxx = 1
-ds = pydicom.read_file(r'O:\DICOM\BMA_Export\Single_Image\Liver.dcm')
-ds2 = pydicom.read_file(r'O:\DICOM\BMA_Export\Single_Image\Bladder.dcm')
-reader = DicomReaderWriter(Contour_Names=['Eye_R'])
-reader.walk_through_folders(r'\\ucsdhc-varis2\Radonc$\DICOM\BMA_Export')
-reader.get_mask()
-xxx = 1
